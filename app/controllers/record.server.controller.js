7f6b240222386bf8ac4671a9ae00592ee7dc52f9
Ashish <ashishcd94@gmail.com>
2021-08-04 15:52:26 +0530

    chore: adds API

--- app/controllers/record.server.controller.js
+++ app/controllers/record.server.controller.js
@@ -4,7 +4,13 @@ const RecordService = require('../services/record.server.service');
 
 exports.get = async (req, res, next) => {
     try {
-        const result = await RecordService.getRecords(req.body);
+
+        if (!req.body.startDate || !req.body.endDate || !req.body.maxCount || !req.body.minCount) {
+            const err = new Error('Missing fields in body');
+            err.status = 400;
+            throw err;
+        }
+        const result = await RecordService.getRecordsByDateAndCount(req.body);
         return res.send({ msg: 'Success', code: 0, result });
     } catch (err) {
         next(err);
--- app/services/record.server.service.js
+++ app/services/record.server.service.js
@@ -5,3 +5,28 @@ const Record = require('../models/record');
 exports.getRecords = (query) => {
     return Record.find(query);
 };
+
+
+exports.getRecordsByDateAndCount = ({ startDate, endDate, minCount, maxCount }) => {
+    return Record.aggregate([
+        {
+            $project: {
+                key: 1,
+                createdAt: 1,
+                totalCount: { $sum: '$counts' }
+            }
+        },
+        {
+            $match: {
+                createdAt: {
+                    $gte: new Date(startDate),
+                    $lte: new Date(endDate)
+                },
+                totalCount: {
+                    $gte: minCount,
+                    $lte: maxCount
+                }
+            }
+        }
+    ]);
+};
